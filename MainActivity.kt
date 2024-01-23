eaeb6bef987e92c86f74a86d1419f34d59410110
Kerrigan Ebey <71675347+BeepDroid@users.noreply.github.com>
2024-01-23 09:24:30 -0600

    Update MainActivity.kt
    Made a seperate class for login logic, this still runs the login page as the first page users see.

--- MainActivity.kt
+++ MainActivity.kt
@@ -18,7 +18,7 @@ import com.example.mealtime1.ui.theme.MealTime1Theme
 import com.example.mealtime1.ui.theme.RegistrationActivity
 
 class MainActivity : ComponentActivity() {
-    private lateinit var editTextUsername: EditText
+   private lateinit var editTextUsername: EditText
     private lateinit var editTextPassword: EditText
     private lateinit var buttonLogin: Button
     private lateinit var buttonRegisterPage : Button
@@ -26,62 +26,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-
-        editTextUsername = findViewById(R.id.editTextUsername)
-        editTextPassword = findViewById(R.id.editTextPassword)
-        buttonLogin = findViewById(R.id.buttonLogin)
-
-        buttonLogin.setOnClickListener {
-            // This is the beginning of our login logic, simple checking.
-            val username = editTextUsername.text.toString()
-            val password = editTextPassword.text.toString()
-
-            // Example: Check if username and password are not empty
-            if (username.isNotEmpty() && password.isNotEmpty()) {
-                // The below stuff is a placeholder for what we actually do when a login is unsuccessful.
-                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show()
-            }
-        }
-        //This is button navigation to the registration page. 
-        buttonRegisterPage.setOnClickListener {
-            // Navigate to RegistrationActivity
-            val intent = Intent(this, RegistrationActivity::class.java)
-            startActivity(intent)
-        }
+        
     }
-}
 
- /*   override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MealTime1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MealTime1Theme {
-        Greeting("Android")
-    }
-}*/
