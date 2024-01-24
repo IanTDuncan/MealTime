62d4fdb307c4617b9d79c1a3f7e6ad37776b8874
Kerrigan Ebey <kebey29@gmail.com>
2024-01-23 18:19:25 -0600

    MealTime Framework
    Committing folder for MealTime framework

---
+++ MealTime1/app/src/main/java/com/example/mealtime1/MyBookmarksActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mealtime1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+
+class MyBookmarksActivity: ComponentActivity() {
+    private lateinit var buttonBackToMainMenu: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.bookmars_activity)
+
+        buttonBackToMainMenu.setOnClickListener {
+            val intent = Intent(this, MainMenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
