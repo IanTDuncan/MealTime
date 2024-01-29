91dc0d5cd536934a3d7dfdf2f7564a0dc92367a4
Kerrigan Ebey <71675347+BeepDroid@users.noreply.github.com>
2024-01-28 21:07:04 -0600

    Add files via upload

---
+++ MealTime1/app/src/main/java/com/example/mealtime1/MealResultActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mealtime1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MealResultActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.meal_result_activity)
+    }
+
+}
