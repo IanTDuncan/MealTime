280efa64966f92b60eff9661fc74f49a1b3f3124
Kerrigan Ebey <71675347+BeepDroid@users.noreply.github.com>
2024-01-28 21:26:27 -0600

    Add files via upload

---
+++ MealTime1/app/src/main/java/com/example/mealtime1/PasswordChangeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mealtime1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class PasswordChangeActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.password_change_activity)
+    }
+
+}
