717f6fad1a152defeb7abffd5f75837c4467f9fa
Cade5480 <143129097+Cade5480@users.noreply.github.com>
2024-04-07 18:58:01 -0500

    File Deletion
    Deleted classes that are no longer being used

--- MealTime1/app/src/main/java/com/example/mealtime1/MyNotifications.kt
+++ MealTime1/app/src/main/java/com/example/mealtime1/MyNotifications.kt
@@ -3,11 +3,11 @@ package com.example.mealtime1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 
-class AccountSupportAction: ComponentActivity() {
+class MyNotifications: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.user_info_activity)
+        setContentView(androidx.core.R.layout.notification_action)
 
     }
 }
